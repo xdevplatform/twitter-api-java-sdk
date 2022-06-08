48e32805e3e4ec87e7cde1ca512cbf4924b0ed61
tmoskovitch <tmoskovitch@twitter.com>
2022-06-08 13:56:59 +1200

    Version 1.2.3

--- src/main/java/com/twitter/clientlib/ApiClient.java
+++ src/main/java/com/twitter/clientlib/ApiClient.java
@@ -225,7 +225,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("twitter-api-java-sdk/1.2.2");
+        setUserAgent("twitter-api-java-sdk/1.2.3");
 
         authentications = new HashMap<String, Authentication>();
     }
